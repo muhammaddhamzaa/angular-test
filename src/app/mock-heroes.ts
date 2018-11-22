2d4028c5f5ea40622df51c3f8f46c0b640d081bb
muhammad.hamza <muhammad.hamza@rolustech.net>
2018-11-22 15:53:55 +0500

    Fixer #2: Added list view with display feature

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,11 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+{ id: 11, name: "NONO" },
+{ id: 12, name: "NANA" },
+{ id: 13, name: "NINI" },
+{ id: 14, name: "NUNU" },
+{ id: 15, name: "NENE" },
+{ id: 16, name: "NSNS" },
+{ id: 17, name: "NDND" }
+];
