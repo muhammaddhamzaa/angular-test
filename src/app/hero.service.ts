060b17a3db91492c3ae158ce5ab17ac412890d13
muhammad.hamza <muhammad.hamza@rolustech.net>
2018-11-23 17:25:34 +0500

    Fixer #6: Added routing to components

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Hero } from './hero';
-import { MessageService } from './message.service';
-import { HEROES } from './mock-heroes';
+
 import { Observable, of } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { MessageService } from './message.service';
+
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+
+  constructor(private messageService: MessageService) { }
+
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: Fetched Heroes');
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
